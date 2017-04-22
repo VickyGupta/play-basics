7bb576e34ada131ba8d7dc4b2ffe7d9272d6cf97
VickyGupta <vicky.gupta@mindtraits.com>
2017-04-22 20:38:26 +0530

    updated

--- app/controllers/AddListController.scala
+++ app/controllers/AddListController.scala
@@ -3,9 +3,28 @@ package controllers
 import javax.inject._
 import play.api._
 
+
+import scala.concurrent.Future
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 import play.modules.reactivemongo._
 import play.api.mvc.{ Action, Controller }
 import reactivemongo.bson.BSONObjectID
+import play.api.libs.json._
+import play.api.libs.json.Json._
+
+
+
+// Reactive Mongo imports
+import reactivemongo.api.Cursor
+
+
+
+
+// BSON-JSON conversions/collection
+import reactivemongo.play.json._
+import play.modules.reactivemongo.json.collection._
 
 import services.AddList
 //import scala.collection.mutable._
@@ -25,12 +44,50 @@ class AddListController @Inject() (add:AddList)(val reactiveMongoApi: ReactiveMo
    * The result is List[Int]. This `Action` is mapped to
    * `GET /get` requests by an entry in the `routes` config file.
    */
-  def add_List = Action {
+
+  def collection: JSONCollection = db.collection[JSONCollection]("sample")
+
+
+
+ 
+  def add_List1 = Action {
+   
     var v= foo
+    
     println(s"Foo: ${v.stringify}")
     Ok(views.html.add(add.addList(5,100) , v.stringify))
    
   }
+
+   def create {
+     println("enter")
+    val json = Json.obj(
+      "name" -> "Vicky",
+      "age" -> 27,
+      "created" -> new java.util.Date().getTime())
+
+      collection.insert(json).onComplete { lastError =>
+        Logger.debug(s"Successfully inserted with LastError: $lastError")
+        Created
+      }
+
+      
+
+    // collection.insert(json).map(lastError =>
+    //   Ok("Mongo LastError: %s".format(lastError)))
+  }
+
+
+   def add_List = Action.async {
+        val cursor: Cursor[JsObject] = collection.find(Json.obj("name" -> "Vicky")).cursor[JsObject]
+
+	        val futureSampleList: Future[List[JsObject]] = cursor.collect[List]()
+	                
+	        futureSampleList.map { sample =>
+	            Ok(toJson(Map("result" -> toJson("success"),"sample" -> toJson(sample))))
+	        }
+   }
+
 
    def foo : BSONObjectID= {
     var id = BSONObjectID.generate
